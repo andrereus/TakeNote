3232e66e83f79438a0e08bfdf170e51b62131af9
Andre Reus <andrereus@users.noreply.github.com>
2023-07-04 14:59:11 +0200

    Create Entity

---
+++ app/src/main/java/com/andre/takenote/Note.kt
@@ -0,0 +1,14 @@
+package com.andre.takenote
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Database Model
+// Entity defines the table in the database
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val text: String
+)
