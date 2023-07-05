54b4c88820c906bc200db08921766e059d043a3b
Andre Reus <andrereus@users.noreply.github.com>
2023-07-05 10:43:37 +0200

    Edit comment

--- app/src/main/java/com/andre/takenote/NoteDatabase.kt
+++ app/src/main/java/com/andre/takenote/NoteDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 // Connect Entity and DAO for Room to use
-// (no migrations for now)
+// (No migrations for now, use in bigger apps / for production)
 @Database(
     entities = [Note::class],
     version = 1
