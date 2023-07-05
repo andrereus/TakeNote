77099da8c076038a007955d930fa2e1b6bd522e1
Andre Reus <andrereus@users.noreply.github.com>
2023-07-05 10:19:50 +0200

    DAO details and comments

--- app/src/main/java/com/andre/takenote/Note.kt
+++ app/src/main/java/com/andre/takenote/Note.kt
@@ -5,8 +5,11 @@ import androidx.room.PrimaryKey
 
 // Database Model
 // Entity defines a table in the database
+// "@Entity" is an annotation that tells room what to do with the provided code
+// "data class" is a class that provides special features like .toString() and more
 @Entity
 data class Note(
+    // Primary key is needed as a unique identifier and will be auto generated for each entry
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
--- app/src/main/java/com/andre/takenote/NoteDao.kt
+++ app/src/main/java/com/andre/takenote/NoteDao.kt
@@ -1,9 +1,27 @@
 package com.andre.takenote
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 // Data Access Object
-// Defines how to interact with the database
+// Defines how to interact with the data in the database
+// "interface" defines which variables and methods a class needs to implement
 @Dao
 interface NoteDao {
+    // "@Upsert" updates if note already exists and inserts if it doesn't exist
+    // "suspend" is for coroutines/asynchronous tasks (no callbacks needed)
+    @Upsert
+    suspend fun upsertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    // Room doesn't provide "read" or "get", so it needs to be implemented with @Query
+    // Flow is used to get realtime updates as soon as something changes in the database
+    // (reactive streams / observables)
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
 }
