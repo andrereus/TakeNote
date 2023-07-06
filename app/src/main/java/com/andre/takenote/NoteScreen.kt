36a062f785c27084d71fa3a4852dccae5d7ba141
Andre Reus <andrereus@users.noreply.github.com>
2023-07-06 20:40:01 +0200

    Fix padding error message

--- app/src/main/java/com/andre/takenote/NoteScreen.kt
+++ app/src/main/java/com/andre/takenote/NoteScreen.kt
@@ -1,5 +1,6 @@
 package com.andre.takenote
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -37,13 +39,14 @@ fun NoteScreen(state: NoteState, onEvent: (NoteEvent) -> Unit) {
             }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
             }
-        }
-    ) { _ ->
+        },
+        modifier = Modifier.padding(16.dp)
+    ) { padding ->
         if (state.isAddingNote) {
             AddNoteDialog(state = state, onEvent = onEvent)
         }
         LazyColumn(
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = padding,
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
