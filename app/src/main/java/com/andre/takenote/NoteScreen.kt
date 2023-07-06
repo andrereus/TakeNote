bda54f193d766d97e43efbdd246f8fd9b95676b2
Andre Reus <andrereus@users.noreply.github.com>
2023-07-06 11:07:35 +0200

    NoteScreen part 2

--- app/src/main/java/com/andre/takenote/NoteScreen.kt
+++ app/src/main/java/com/andre/takenote/NoteScreen.kt
@@ -3,16 +3,20 @@ package com.andre.takenote
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -67,6 +72,29 @@ fun NoteScreen(
                             )
                             Text(text = sortType.name)
                         }
+                    }
+                }
+            }
+            items(state.notes) {note ->
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = note.title,
+                            fontSize = 20.sp
+                        )
+                        Text(text = note.text, fontSize = 12.sp)
+                    }
+                    IconButton(onClick = {
+                        onEvent(NoteEvent.DeleteNote(note))
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete note"
+                        )
                     }
                 }
             }
