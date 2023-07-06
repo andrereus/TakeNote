6eec3f951f0d217bc672e40c0dee8ee099b61054
Andre Reus <andrereus@users.noreply.github.com>
2023-07-06 12:05:46 +0200

    MainActivity

--- app/src/main/java/com/andre/takenote/MainActivity.kt
+++ app/src/main/java/com/andre/takenote/MainActivity.kt
@@ -3,21 +3,47 @@ package com.andre.takenote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 import com.andre.takenote.ui.theme.TakeNoteTheme
 
 class MainActivity : ComponentActivity() {
+    // To avoid initializing the database here, use dependency injection like Dagger Hilt
+    // (Recommended for bigger apps / for production)
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            NoteDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+    // To avoid using a factory, also use something like Dagger Hilt
+    private val viewModel by viewModels<NoteViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return NoteViewModel(db.dao) as T
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TakeNoteTheme {
-                // TODO
+                val state by viewModel.state.collectAsState()
+                NoteScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
--- app/src/main/java/com/andre/takenote/NoteScreen.kt
+++ app/src/main/java/com/andre/takenote/NoteScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -42,7 +43,8 @@ fun NoteScreen(
                     contentDescription = "Add note"
                 )
             }
-        }
+        },
+        modifier = Modifier.padding(16.dp)
     ) { padding ->
         if(state.isAddingNote) {
             AddNoteDialog(state = state, onEvent = onEvent)
