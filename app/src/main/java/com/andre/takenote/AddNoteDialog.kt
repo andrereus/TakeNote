92f0ffe1cf6fd1086c7f8eef8419f4943083f4ce
Andre Reus <andrereus@users.noreply.github.com>
2023-07-06 11:32:41 +0200

    NoteDialog

---
+++ app/src/main/java/com/andre/takenote/AddNoteDialog.kt
@@ -0,0 +1,67 @@
+package com.andre.takenote
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddNoteDialog(
+    state: NoteState,
+    onEvent: (NoteEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = {
+            onEvent(NoteEvent.HideDialog)
+        },
+        title = { Text(text = "Add note") },
+        text = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                TextField(
+                    value = state.title,
+                    onValueChange = {
+                        onEvent(NoteEvent.SetTitle(it))
+                    },
+                    placeholder = {
+                        Text(text = "Title")
+                    }
+                )
+                TextField(
+                    value = state.text,
+                    onValueChange = {
+                        onEvent(NoteEvent.SetText(it))
+                    },
+                    placeholder = {
+                        Text(text = "Text")
+                    }
+                )
+            }
+        },
+        confirmButton = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Button(onClick = {
+                    onEvent(NoteEvent.SaveNote)
+                }) {
+                    Text(text = "Save")
+                }
+            }
+        }
+    )
+}
