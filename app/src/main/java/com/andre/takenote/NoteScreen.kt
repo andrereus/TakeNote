f50aef258f1d5a56b0a94836b6b439cfd8998d0f
Andre Reus <andrereus@users.noreply.github.com>
2023-07-06 21:28:15 +0200

    Change radio buttons to dropdown

--- app/src/main/java/com/andre/takenote/NoteScreen.kt
+++ app/src/main/java/com/andre/takenote/NoteScreen.kt
@@ -1,8 +1,7 @@
 package com.andre.takenote
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,19 +9,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,9 +35,7 @@ import androidx.compose.ui.unit.sp
 fun NoteScreen(state: NoteState, onEvent: (NoteEvent) -> Unit) {
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                onEvent(NoteEvent.ShowDialog)
-            }) {
+            FloatingActionButton(onClick = { onEvent(NoteEvent.ShowDialog) }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
             }
         },
@@ -43,46 +44,46 @@ fun NoteScreen(state: NoteState, onEvent: (NoteEvent) -> Unit) {
         if (state.isAddingNote) {
             AddNoteDialog(state = state, onEvent = onEvent)
         }
-        LazyColumn(
-            contentPadding = padding,
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .horizontalScroll(rememberScrollState()),
-                    verticalAlignment = Alignment.CenterVertically
+
+        var expanded by remember { mutableStateOf(false) }
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box {
+                TextButton(onClick = { expanded = true }) {
+                    Text(state.sortType.name)
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
                 ) {
                     SortType.values().forEach { sortType ->
-                        Row(
-                            modifier = Modifier.clickable {
+                        DropdownMenuItem(
+                            text = { Text(text = sortType.name) },
+                            onClick = {
                                 onEvent(NoteEvent.SortNotes(sortType))
-                            },
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            RadioButton(
-                                selected = state.sortType == sortType,
-                                onClick = {
-                                    onEvent(NoteEvent.SortNotes(sortType))
-                                }
-                            )
-                            Text(text = sortType.name)
-                        }
+                                expanded = false
+                            })
                     }
                 }
             }
-            items(state.notes) { note ->
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(text = note.title, fontSize = 20.sp)
-                        Text(text = note.text, fontSize = 12.sp)
-                    }
-                    IconButton(onClick = {
-                        onEvent(NoteEvent.DeleteNote(note))
-                    }) {
-                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete note")
+
+            LazyColumn(
+                contentPadding = padding,
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(state.notes) { note ->
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Column(modifier = Modifier.weight(1f)) {
+                            Text(text = note.title, fontSize = 20.sp)
+                            Text(text = note.text, fontSize = 12.sp)
+                        }
+                        IconButton(onClick = { onEvent(NoteEvent.DeleteNote(note)) }) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete note"
+                            )
+                        }
                     }
                 }
             }
